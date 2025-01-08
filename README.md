8604e43f2e0cae8ea4459643a8d3ada4b2cfeed9
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-01-08 18:55:17 -0500

    documentation link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[Python 3.13.1 documentation](https://docs.python.org/3/) \
+
 Harvard CS50’s Introduction to Programming with Python | 
 [Notes](https://cs50.harvard.edu/python/2022/) | 
 [Video](https://www.youtube.com/watch?v=nLRL_NcnK-4&list=PLG89yezH97iysjKYEMX96QNefiZUlNtRz&index=23)
--- hello.py
+++ hello.py
@@ -8,17 +8,26 @@ comments
 ### FUNCTIONS
 # print("Hello World")
 
+# *** printing quotes ""
+# print('Hello, "friend"')
+# print('Hello, \"friend\"')
+
+
 ### VARIABLES
-# name=input("What's your name? ")
+name=input("What's your name? ")
 # print(name)
 
 ### + concatenates
 # print("Hello, " + name)
 
+### f"{variable}"
+print(f"Hello, {name}")
+
 ### ARGUMENTS
-argument1="first argument"
-argument2="second argument"
-argument3="third argument"
+# argument1="first argument"
+# argument2="second argument"
+# argument3="third argument"
 
-print(argument1, argument2, argument3, sep=", ",end=".")
+# print(argument1, argument2, argument3, sep=", ",end=".")
 
+###
