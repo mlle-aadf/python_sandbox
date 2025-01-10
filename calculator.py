00d4347f4ff1deedfca0e3e5ccfebf2656e55396
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-01-10 14:01:05 -0500

    end lecture 0

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
+def main():
+    x = int(input("What's x? "))
+    
+    print("x squared is", square(x))
+
+def square(n):
+    # return n**2
+    # or 
+    return pow(n, 2)
+
+main()
+
+
 ### int(), float(), round()
 
 # x=int(input("What's x? "))
@@ -6,20 +19,20 @@
 # x=float(input("What's x? "))
 # y=float(input("What's y? "))
 
-x=float(input("What's x? "))
-y=float(input("What's y? "))
-z=round(x+y)
+# x=float(input("What's x? "))
+# y=float(input("What's y? "))
+# z=round(x+y)
 # print(z)
 
 # *** round(number[, ndigits]) , default: nearest int
 # https://docs.python.org/3/library/functions.html#round
 
 # z=round(x+y, 4)
-print(z)
+# print(z)
 
 # *** format print(z) output, f-strings
 # https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
-variable = z
-print(f"Variable: {variable}")
-print(f"{z:,}")
-print(f"{z:.3f}") # "print 3 digits of float"
+# variable = z
+# print(f"Variable: {variable}")
+# print(f"{z:,}")
+# print(f"{z:.3f}") # "print 3 digits of float"
