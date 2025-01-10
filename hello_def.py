67eb9f73c884c60da9cdee7d783ebdae3349c5f3
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-01-10 14:01:05 -0500

    end lecture 1

--- hello_def.py
+++ hello_def.py
@@ -1,14 +1,22 @@
 ### DEFINING FUNCTIONS >>> def functionName(parameter):
-def bonjour():
+def main():
     name = input("C'est quoi ton nom? ")
+    bonjour(name)
+
+
+
+def bonjour(name="toi"):
+    # name = input("C'est quoi ton nom? ")
     print(f"Bonjour, {name} :)")
 
-bonjour()
+# bonjour()
 
-def bye(name="default"):
-    print(f"Bye, {name}!")
+# def bye(name="default"):
+#     print(f"Bye, {name}!")
 
-bye()
+# bye()
 
-name=input("Remind me what your name is again? ")
-bye(name)
+# name=input("Remind me what your name is again? ")
+# bye(name)
+
+main()
