a4dcadd76c108886b3c88a9535a9a6953fcb448e
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-01-12 01:05:37 -0500

    completed Deep Thought

--- cs50/problemSets/pset1/deepThought.py
+++ cs50/problemSets/pset1/deepThought.py
@@ -2,8 +2,31 @@
 # https://cs50.harvard.edu/python/2022/psets/1/deep/
 
 # Implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
-
 # Be sure to vary the casing of your input and “accidentally” add spaces on either side of your input before pressing enter. Your program should behave as expected, case- and space-insensitively.
+
+import re
+
+def main():
+    answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+    check_answer(answer)
+    
+
+def clean_answer(input_answ):
+    if input_answ.isdigit():
+        return int(input_answ)
+    else:
+        # Use re.sub() to replace spaces and special characters with an empty string
+        cleaned_string = re.sub(r'[^A-Za-z0-9]', '', input_answ)
+        lowered_string = cleaned_string.lower()
+        return lowered_string
+
+def check_answer(answer):
+    cleaned = clean_answer(answer)
+    acceptable = {42, "fortytwo"}
+    verdict = "Yes" if cleaned in acceptable else "No"
+    print(verdict)
+
+main()
 
 # [42] should output: Yes
 # [Forty-Two] should output: Yes
